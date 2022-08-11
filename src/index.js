e16fbdc4325c5061fcc3abacc99f20402ce508cb
Sasha <volfym79@gmail.com>
2022-08-11 12:04:53 +0200

    start form style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+
+// const add_new = document.querySelector('.add-new-button');
+// const form = document.querySelector('.form-section');
+
+// add_new.addEventListener("click", () => form.classList.toggle('form-open'));
