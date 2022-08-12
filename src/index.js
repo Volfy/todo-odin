b2c0adf998e37d2cc967babc0db74ae0a29e2eb8
Sasha <volfym79@gmail.com>
2022-08-12 06:05:13 +0200

    style forms. add basic tabbing to form display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import './style.css';
 
-// const add_new = document.querySelector('.add-new-button');
-// const form = document.querySelector('.form-section');
+const add_new = document.querySelector('.add-new-button');
+const form = document.querySelector('.form-section');
 
-// add_new.addEventListener("click", () => form.classList.toggle('form-open'));
+const formtodo = document.querySelector('.add-todo');
+const todobtn = document.querySelector('#to-do-tab');
+const formproject = document.querySelector('.add-project');
+const projectbtn = document.querySelector('#project-tab');
+const closebtn = document.querySelector('#close-tab');
+
+add_new.addEventListener("click", () => {
+    form.classList.toggle('form-open')
+    formtodo.classList.add('current');
+});
+
+todobtn.addEventListener("click", () => {
+    formproject.classList.remove("current");
+    
+    formtodo.classList.add("current");
+    
+});
+projectbtn.addEventListener("click", () => {
+    formtodo.classList.remove("current");
+    formproject.classList.add("current");
+    
+});
+closebtn.addEventListener("click", () => {
+    formproject.classList.remove('current');
+    formtodo.classList.remove('current');
+    form.classList.remove('form-open');
+    
+    
+});
